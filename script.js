fb9bcb124eae59019af4b13fa3b62d2c061b60f3
Jose Arniel Pama <pamajosearniel@gmail.com>
2025-02-06 16:51:37 +0800

    add VoiceRSS.speech function call; play an audio based on the given text

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const button = document.getElementById("button");
+const audioElement = document.getElementById("audio");
+
 // VoiceRSS Javascript SDK
 const VoiceRSS = {
   speech: function (e) {
@@ -101,3 +104,18 @@ const VoiceRSS = {
     throw "The browser does not support HTTP request";
   },
 };
+
+function test() {
+  VoiceRSS.speech({
+    key: "e614ba096f8a41329f811f24625a7786",
+    src: "Hello, world!",
+    hl: "en-us",
+    v: "Linda",
+    r: 0,
+    c: "mp3",
+    f: "44khz_16bit_stereo",
+    ssml: false,
+  });
+}
+
+test();
